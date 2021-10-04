3e3c3732e333df665e8783cd189c04aefb8b8dbf
Bhavig Pointi <p.bhavig@gmail.com>
2021-10-04 00:43:11 -0400

    tipster p2 finished

---
+++ Tipster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Tipster
+//
+//  Created by Bhavig Pointi on 10/3/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+
+    @IBOutlet weak var posterView: UIImageView!
+}
